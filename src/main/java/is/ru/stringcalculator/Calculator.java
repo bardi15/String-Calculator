0c0e65bf51cba1c91cbaf014f0a21e1b484a8992
Barði Þorsteinsson <bardi15@ru.is>
2016-10-11 13:04:33 +0000

    lagfæringar á uppsetningu

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,24 @@
+package is.ru.stringcalculator;
+
+public class Calculator 
+{
+
+	public static int Add(String numbers)
+	{
+		if (numbers.equals(""))
+		{
+			return 0;
+		}
+		String[] data = numbers.split(",");
+		int num = 0;
+		for (int k = 0; k < data.length; k++)
+		{
+			num += Integer.parseInt(data[k]);
+		}
+		System.out.println((num));
+		return num;
+	}
+
+
+
+}
