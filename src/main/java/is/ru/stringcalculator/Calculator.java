e6d2cea76036c6100de02bb1da731e06206e33d4
Barði Þorsteinsson <bardi15@ru.is>
2016-10-11 16:44:56 +0000

    passed test testNegative

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,31 +1,59 @@
 package is.ru.stringcalculator;
 
+import java.util.ArrayList;
+
+
 public class Calculator 
 {
 
-	public static int add(String numbers)
+	public static int add(String numbers) throws IllegalArgumentException
 	{
 		if (numbers.equals(""))
 		{
 			return 0;
 		}
-		if ( numbers.contains((",")) )
+
+		String[] data = SplitNumbers(numbers);
+		//int [] negativeList = new int [data.length];
+		String negativeString = "";
+		ArrayList<Integer> negNums = new ArrayList<Integer>();
+		int sum = 0;
+		for (int k = 0; k < data.length; k++)
 		{
-			String[] data = SplitNumbers(numbers);
-			int num = 0;
-			for (int k = 0; k < data.length; k++)
+			int currNum = ParseToInt(data[k]);
+			
+			if (currNum < 0)
 			{
-				num += ParseToInt(data[k]);
+				negNums.add(currNum);
 			}
-			return num;
+			else
+			{
+				sum = sum + currNum;
+			}
 		}
-		else
+		
+		if (negNums.size() > 0)
 		{
-			return ParseToInt(numbers);
+			throw new IllegalArgumentException("Negatives not allowed: " + CreateNegativeString(negNums));
 		}
+		
+		return sum;
 
 	}
-	
+
+	public static String CreateNegativeString(ArrayList<Integer> negNums)
+	{
+		String negString = "";
+		for (int i = 0; i < negNums.size() - 1; i++)
+		{
+			negString += negNums.get(i) + ",";
+		}
+		
+		negString += negNums.get(negNums.size() - 1);
+		
+		return negString;
+		
+	}
 	
 	public static int ParseToInt(String number)
 	{
@@ -36,7 +64,15 @@ public class Calculator
 	{
 		return numbers.split(",|\\n");
 	}
-
-
-
+	
+	public static boolean CheckNegative (int numbers)
+	{
+		if (numbers < 0)
+		{
+			System.out.println("ITS NEGATIVE");
+			return true;
+		}
+		
+		return false;
+	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.hamcrest.Matcher;
+
 public class CalculatorTest {
 
 	public static void main(String args[]) 
@@ -11,6 +15,9 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
 	public void testEmptyString() 
 	{
@@ -40,5 +47,19 @@ public class CalculatorTest {
 	{
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
+
+	@Test
+    public void testFunkyNewLine()
+	{
+    	assertEquals(1, Calculator.add("1,\n"));
+    }	
 	
+	@Test
+    public void testNegative()
+	{
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Negatives not allowed: -4,-5");
+		
+    	Calculator.add("-4,-5");
+    }	
 }
